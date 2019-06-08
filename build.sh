1792cec449c88003e8a1e05808abe5b7e4fbca4b
vrunoa <vruno.alassia@gmail.com>
2019-06-07 19:03:57 -0700

    add script to build modules

---
+++ build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+adb_repl() {
+    pushd appium-adb
+    npm link
+    popd
+    pushd appium-adb-repl
+    npm link appium-adb
+    popd
+}
+
+appium() {
+    pushd appium-adb
+    npm link
+    popd
+    pushd appium-android-driver
+    npm link appium-adb
+    npm link
+    popd
+    pushd appium-base-driver
+    npm link
+    popd
+    pushd appium
+    npm link appium-adb
+    npm link appium-android-driver
+    npm link appium-base-driver
+    popd
+}
+
+while :
+do
+    case "$1" in
+        adb_repl)
+            adb_repl;
+            exit 0
+            ;;
+        appium)
+            appium;
+            exit 0
+            ;;
+        *)
+            help
+            break
+            ;;
+    esac
+done
